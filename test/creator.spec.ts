129a9ed0bdbda62fc0bcb15300d688a2ab531b04
Samuele Marro <marrosamuele@gmail.com>
2022-03-11 18:46:14 +0100

    Added mint tests.

--- test/creator.spec.ts
+++ test/creator.spec.ts
@@ -14,9 +14,6 @@ let bob: any;
 let charlie: any;
 let contract: any;
 
-const firstTokenId = '5042';
-const secondTokenId = '79217';
-
 describe('test CreatorToken', function () {
     before(async function () {
         provider = vite.localProvider();
@@ -44,11 +41,73 @@ describe('test CreatorToken', function () {
     describe('createToken', function() {
         it('creates a token', async function() {
             await contract.call('createToken', [154], {caller: alice});
+            expect(await contract.query('exists', [alice.address], {caller: alice})).to.be.equal(true);
+            expect(await contract.query('tokenCoefficient', [alice.address], {caller: alice})).to.be.equal(154);
+            expect(await contract.query('balanceOf', [alice.address, alice.address], {caller: alice})).to.be.equal(10000);
+            expect(await contract.query('totalSupply', [alice.address], {caller: alice})).to.be.equal(10000);
+            expect(await contract.query('tradableSupply', [alice.address], {caller: alice})).to.be.equal(0);
+            expect(await contract.query('currentPrice', [alice.address], {caller: alice})).to.be.equal(0);
+
+            // TODO: Check events
         });
 
         it('fails to create a token with an odd coefficient', async function() {
             await expect(
                 contract.call('createToken', [153], {caller: alice})
+            ).to.eventually.be.rejectedWith('revert');
+        });
+
+        it('fails to create a token with coefficient 0', async function() {
+            await expect(
+                contract.call('createToken', [0], {caller: alice})
+            ).to.eventually.be.rejectedWith('revert');
+        });
+
+        it('fails to create a token twice', async function() {
+            await contract.call('createToken', [154], {caller: alice});
+            await expect(
+                contract.call('createToken', [154], {caller: alice})
+            ).to.eventually.be.rejectedWith('revert');
+        });
+    })
+
+    describe('mint', function() {
+        it('mints a token', async function() {
+            await deployer.sendToken(alice.address, '1000000');
+            await contract.call('createToken', [154], {caller: alice});
+            // Mint 27 tokens
+            // \int_0^27 154x dx = 56133
+            await contract.call('mint', [alice.address, 27], {caller: alice, tokenId: 'tti_5649544520544f4b454e6e40', amount: '56133'});
+            expect(await contract.balance()).to.be.equal(56133);
+            expect(await alice.balance()).to.be.equal(1000000 - 56133);
+
+            expect(await contract.query('balanceOf', [alice.address, alice.address], {caller: alice})).to.be.equal(10000 + 27);
+            expect(await contract.query('totalSupply', [alice.address], {caller: alice})).to.be.equal(10000 + 27);
+            expect(await contract.query('tradableSupply', [alice.address], {caller: alice})).to.be.equal(27);
+            expect(await contract.query('currentPrice', [alice.address], {caller: alice})).to.be.equal(154 * 27);
+
+            // Mint 10 tokens
+            // \int_27^37 154x dx = 49280
+            await contract.call('mint', [alice.address, 10], {caller: alice, tokenId: 'tti_5649544520544f4b454e6e40', amount: '49280'});
+            expect(await contract.balance()).to.be.equal(56133 + 49280);
+            expect(await alice.balance()).to.be.equal(1000000 - (56133 + 49280));
+
+            expect(await contract.query('balanceOf', [alice.address, alice.address], {caller: alice})).to.be.equal(10000 + 27 + 10);
+            expect(await contract.query('totalSupply', [alice.address], {caller: alice})).to.be.equal(10000 + 27 + 10);
+            expect(await contract.query('tradableSupply', [alice.address], {caller: alice})).to.be.equal(27 + 10);
+            expect(await contract.query('currentPrice', [alice.address], {caller: alice})).to.be.equal(154 * (27 + 10));
+        });
+
+        it('fails to mint a non-existent token', async function() {
+            await expect(
+                contract.call('mint', [alice.address, 0], {caller: alice, tokenId: 'tti_5649544520544f4b454e6e40', amount: '56133'})
+            ).to.eventually.be.rejectedWith('revert');
+        });
+
+        it('fails to mint 0 tokens', async function() {
+            await contract.call('createToken', [154], {caller: alice});
+            await expect(
+                contract.call('mint', [alice.address, 0], {caller: alice, tokenId: 'tti_5649544520544f4b454e6e40', amount: '56133'})
             ).to.eventually.be.rejectedWith('revert');
         });
     })
